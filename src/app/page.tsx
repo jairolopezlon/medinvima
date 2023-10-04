44a8ec49e52ace1c587ed37baed4e71311f1ca0b
jairolopezlon <jairolop88@gmail.com>
2023-10-04 06:58:38 -0500

    fix: apply prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-    return (
-        <div className="flex justify-center items-center h-[100vh]">
-            <h1 className="text-3xl">Medinvima</h1>
-        </div>
-    );
+export default function Home(): React.JSX.Element {
+  return (
+    <div className='flex justify-center items-center h-[100vh]'>
+      <h1 className='text-3xl'>Medinvima</h1>
+    </div>
+  )
 }
