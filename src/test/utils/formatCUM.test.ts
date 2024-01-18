f94987236f1950675b491185bbd12c15660c7971
jairolopezlon <jairolop88@gmail.com>
2024-01-18 02:29:50 -0500

    chore: add mock cum vigentes and use it in test

--- src/test/utils/formatCUM.test.ts
+++ src/test/utils/formatCUM.test.ts
@@ -1,15 +1,14 @@
+import { cumVigentes } from '@/mocks/cumVigentes'
 import { formatCUM } from '@/utils'
 
 describe('function to format CUM', () => {
   it('should return a string 20 length', () => {
-    const expediente = '19942122'
-    const consecutivo = '24'
-    const atc = 'A02BC01'
+    const [item1] = cumVigentes
 
     const result = formatCUM({
-      atc,
-      consecutivo,
-      expediente,
+      atc: item1.atc,
+      consecutivo: item1.consecutivocum,
+      expediente: item1.expediente,
     })
 
     const CUM_LENGTH = 20
@@ -18,11 +17,13 @@ describe('function to format CUM', () => {
   })
 
   it('should have the correct format', () => {
-    const expediente = '15678'
-    const consecutivo = '1'
-    const atc = 'A02BC01'
+    const [, item2] = cumVigentes
 
-    const result = formatCUM({ atc, consecutivo, expediente })
+    const result = formatCUM({
+      atc: item2.atc,
+      consecutivo: item2.consecutivocum,
+      expediente: item2.expediente,
+    })
 
     expect(result).toMatch(/^\d{8}-\d{2}-\w{8}$/u)
   })
