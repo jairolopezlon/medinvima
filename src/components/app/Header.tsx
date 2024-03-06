d655a012cf1bbb263bce62dd62e09ca53484935a
jairolopezlon <jairolop88@gmail.com>
2024-03-06 05:11:30 -0500

    feat: add conditional to display link

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -2,10 +2,12 @@
 import Button from '../atoms/Button'
 import Link from 'next/link'
 import { UNDEFINED } from '@/constants'
+import { usePathname } from 'next/navigation'
 import { useSessionContext } from '@/contexts/Session'
 
 export default function AppHeader(): React.JSX.Element {
   const { logout, userEmail, isAuthenticated } = useSessionContext()
+  const pathname = usePathname()
 
   const handleLogout = (): void => {
     if (userEmail !== UNDEFINED) {
@@ -25,9 +27,11 @@ export default function AppHeader(): React.JSX.Element {
       </div>
       {isAuthenticated ? (
         <div className='flex gap-2 items-center'>
-          <Link className='text-sm max-sm:text-xs text-indigo-500 font-semibold hover:text-indigo-700' href='/app'>
-            Ir a App
-          </Link>
+          {pathname === '/app' ? null : (
+            <Link className='text-sm max-sm:text-xs text-indigo-500 font-semibold hover:text-indigo-700' href='/app'>
+              Ir a App
+            </Link>
+          )}
           <Button
             className='font-semibold text-sm max-sm:text-xs  text-indigo-500 hover:text-indigo-700'
             level='tertiary'
